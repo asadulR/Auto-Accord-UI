bfb560519b983f26a68ec27b6ea2e8a4ea1cdb73
asadulR <asad1252001@gmail.com.com>
2022-05-01 15:49:15 +0600

    error fixing commit

--- src/Components/Pages/Shared/Auth/Firebase.init.js
+++ src/Components/Pages/Shared/Auth/Firebase.init.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
-  apiKey: "AIzaSyC588oKxcTSYVX92-tIp_6Vk6rQ3R8yP00",
-  authDomain: "auto-accord-10e53.firebaseapp.com",
-  projectId: "auto-accord-10e53",
-  storageBucket: "auto-accord-10e53.appspot.com",
-  messagingSenderId: "439734757377",
-  appId: "1:439734757377:web:d3d76e679c15b14ca97800"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
@@ -14,3 +14,5 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 export default auth;
+
+
